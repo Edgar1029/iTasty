7a55cc90e5cbd5a8d9296c493d05596783bc25a6
ChaoXky <bob09357@gmail.com>
2024-05-19 15:38:32 +0800

    調整版控 暫時移除

--- project_itasty/project_itasty/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace project_itasty.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
