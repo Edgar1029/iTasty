483cdc16f920314e2cff2901489f16a514d1c200
ChaoXky <bob09357@gmail.com>
2024-05-20 16:21:23 +0800

    123

---
+++ project_itasty/project_itasty/Controllers/HomeController1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project_itasty.Models;
+
+namespace project_itasty.Controllers
+{
+	public class HomeController1 : Controller
+	{
+		private readonly ITastyDbContext _context;
+
+		public HomeController1(ITastyDbContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
