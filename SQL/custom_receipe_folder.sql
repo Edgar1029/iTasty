208401891c4062b14cc12ac12742f367ac41b7a1
Alan55-maker <78891954+Alan55-maker@users.noreply.github.com>
2024-05-26 22:53:38 +0800

    Delete SQL/custom_receipe_folder.sql

--- SQL/custom_receipe_folder.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE custom_receipe_folder (
-    member_id INT,
-    custom_folder_id INT IDENTITY,
-    custom_folder_name NVARCHAR(50) NOT NULL,
-    recipe_id INT NOT NULL,
-	primary key(member_id,custom_folder_id)
-);
