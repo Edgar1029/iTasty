2a125d8cdd0775a28de9721d6b95e7278d25e011
ChaoXky <bob09357@gmail.com>
2024-05-27 14:49:49 +0800

    更新userinfo 控制器 get路徑

--- project_itasty/project_itasty/Controllers/UserInfoesController.cs
+++ project_itasty/project_itasty/Controllers/UserInfoesController.cs
@@ -9,36 +9,41 @@ using project_itasty.Models;
 
 namespace project_itasty.Controllers
 {
-	public class UserInfoesController : Controller
-	{
-		private readonly ITastyDbContext _context;
+    public class UserInfoesController : Controller
+    {
+        private readonly ITastyDbContext _context;
 
-		public UserInfoesController(ITastyDbContext context)
-		{
-			_context = context;
-		}
+        public UserInfoesController(ITastyDbContext context)
+        {
+            _context = context;
+        }
 
-		// GET: UserInfoes
-		[HttpGet("{id}")]
-		public IActionResult Index(int id)
-		{
-			var userinfo = _context.UserInfos.Find(id);
+        // GET: UserInfoes
+        [HttpGet("user/{email}")]
+        public IActionResult Index(string email)
+        {
+            //var userinfo = _context.UserInfos.Find(id);
+            var select_userinfo = from obj in _context.UserInfos
+                                  where obj.Email.IndexOf(email+'@') == 0
+                                  select obj;
 
-			var query = from o in _context.UserFollowers
-						   where o.UserId == id
-						   join u in _context.UserInfos on o.FollowerId equals u.UserId
-						   orderby o.FollowDate descending
-						   select new { o, u };
+            UserInfo user_info = select_userinfo.First();
 
-			ViewBag.user = query;
+            var query = from o in _context.UserFollowers
+                        where o.UserId == user_info.UserId
+                        join u in _context.UserInfos on o.FollowerId equals u.UserId
+                        orderby o.FollowDate descending
+                        select new { o, u };
+
+            ViewBag.user = query;
             foreach (var item in query)
             {
                 var count = from c in _context.UserFollowers
-							where c.UserId == item.u.UserId
-							select c;
-				ViewData[$"follower_{item.u.UserId}"] = count;
+                            where c.UserId == item.u.UserId
+                            select c;
+                ViewData[$"follower_{item.u.UserId}"] = count;
             }
-            return View(userinfo);
-		}
-	}
+            return View(user_info);
+        }
+    }
 }
