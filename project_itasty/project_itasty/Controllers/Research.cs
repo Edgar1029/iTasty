509f552a9feb49237f2279adf5f576301e2c2e77
Stanley Lee <aaaaaapley12@gmail.com>
2024-05-28 22:44:34 +0800

    新增篩選頁畫面

---
+++ project_itasty/project_itasty/Controllers/Research.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace project_itasty.Controllers
+{
+    public class Research : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
