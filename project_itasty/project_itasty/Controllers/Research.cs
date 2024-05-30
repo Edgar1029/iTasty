7c07eb478ca25cdd57cf3c8e369722989802d5a7
apley <aaaaaapley12@gmail.com>
2024-05-30 17:00:41 +0800

    增加資料表外來鍵

--- project_itasty/project_itasty/Controllers/Research.cs
+++ project_itasty/project_itasty/Controllers/Research.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using project_itasty.Models;
+using System.Linq;
 
 namespace project_itasty.Controllers
 {
@@ -12,9 +14,22 @@ namespace project_itasty.Controllers
             _context = context;
         }
 
-        public IActionResult Index(string time_option, string food, string search_type, string search)
+        public IActionResult Index(string time_option, string food,string meat,string vegetable,string type, string search_type, string search)
         {
-            var recipes = _context.RecipeTables.AsQueryable();
+            var recipes = from r in _context.RecipeTables
+                          join u in _context.UserInfos on r.UserId equals u.UserId
+                          select new {
+                                    Id=r.Id,
+                                    Name=r.Name,
+                                    UserName=u.UserName,
+                                    TimesWatched=r.TimesWatched,
+                                    ProteinUsed = r.ProteinUsed,
+                                    MealType = r.MealType,
+                                    HealthyOptions=r.HealthyOptions,
+                                    CuisineStyle = r.CuisineStyle,
+                                    RecipeIntroduction=r.RecipeIntroduction,
+                          }; // 只選擇 RecipeTable 對象
+
 
             // 根據時間選項過濾
             if (!string.IsNullOrEmpty(time_option) && time_option != "all")
@@ -41,7 +56,25 @@ namespace project_itasty.Controllers
             // 根據食材選項過濾
             if (!string.IsNullOrEmpty(food) && food != "all")
             {
-                recipes = recipes.Where(r => r.MealType.Contains(food));
+                recipes = recipes.Where(r => r.ProteinUsed.Contains(food));
+            }
+
+            // 根據餐類選項過濾
+            if (!string.IsNullOrEmpty(meat) && meat != "all")
+            {
+                recipes = recipes.Where(r => r.MealType.Contains(meat));
+            }
+
+            // 根據膳食選項過濾
+            if (!string.IsNullOrEmpty(vegetable) && vegetable != "all")
+            {
+                recipes = recipes.Where(r => r.HealthyOptions.Contains(vegetable));
+            }
+
+            // 根據菜式選項過濾
+            if (!string.IsNullOrEmpty(type) && type != "all")
+            {
+                recipes = recipes.Where(r => r.CuisineStyle.Contains(type));
             }
 
             // 根據搜索類型和搜索關鍵字過濾
@@ -53,7 +86,7 @@ namespace project_itasty.Controllers
                 }
                 else if (search_type == "author")
                 {
-                    recipes = recipes.Where(r => r.Name.Contains(search));
+                    recipes = recipes.Where(r => r.UserName.Contains(search));
                 }
             }
 
