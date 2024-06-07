20de524d4154de4e918f604c37f3a96e66ff74af
apley <aaaaaapley12@gmail.com>
2024-06-07 23:12:20 +0800

    成功灌資料進自建模型

--- project_itasty/project_itasty/Controllers/Background-control.cs
+++ project_itasty/project_itasty/Controllers/Background-control.cs
@@ -1,12 +1,62 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using project_itasty.Models;
+using System.Linq;
 
 namespace project_itasty.Controllers
 {
-    public class background_control : Controller
+    public class Background_control : Controller
     {
-        public IActionResult Index()
+        private readonly ITastyDbContext _context;
+        public Background_control(ITastyDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> index()
+        {
+            #region Chart 圖表
+            var chartData1 = new Background_Control_ChartData
+            {
+                Labels = await _context.RecipeTables.Select(r => r.RecipeName).ToListAsync(),
+                Data = await _context.RecipeTables.Select(r => r.Views).ToListAsync()
+            };
+
+            var chartData2 = new Background_Control_ChartData
+            {
+                Labels = new List<string> { "January", "February", "March", "April", "May", "June" },
+                Data = new List<int> { 12, 19, 3, 5, 2, 3 }
+            };
+            #endregion
+
+            #region 從recipedTable拿資料，並把資料灌進自建的模型(Background_Control_RecipedTable)
+            var RecipedTable = new List<Background_Control_RecipedTable>();
+
+            var recipeList = _context.RecipeTables.OrderByDescending(r => r.Views).ToList();
+
+            foreach (var recipe in recipeList)
+            {
+                RecipedTable.Add(new Background_Control_RecipedTable
+                {
+                    RecipedName = recipe.RecipeName,
+                    Author = recipe.UserId, 
+                    RecipedView = recipe.Views, 
+                    NumberOfComment = recipe.Favorites,
+                    RecipedStatus = recipe.RecipeStatus
+                });
+            }
+            #endregion
+
+
+            var model =  new Backgroud_Control_Model
+            {
+                ChartViewsData = chartData1,
+                ChartMembershipData = chartData2,
+                RecipedTable = RecipedTable,
+            };
+
+            return View(model);
+
         }
     }
 }
---
+++ project_itasty/project_itasty/Models/Backgroud_Control_Model.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace project_itasty.Models
+{
+    public class Backgroud_Control_Model
+    {
+        public Background_Control_ChartData? ChartViewsData { get; set; }
+        public Background_Control_ChartData? ChartMembershipData{ get; set; }
+
+        public List<Background_Control_RecipedTable>? RecipedTable { get; set; }
+
+    }
+
+    public class Background_Control_ChartData
+    {
+        public List<string> Labels { get; set; }
+        public List<int> Data { get; set; }
+    }
+
+    public class Background_Control_RecipedTable 
+    {
+        public string RecipedName { get; set; }
+        public int Author { get; set; }
+        public int RecipedView { get; set; }
+        public int NumberOfComment { get; set; }
+        public string RecipedStatus { get; set; }
+    }
+}
